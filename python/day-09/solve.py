c110086682a8c36a16c1da66ce61a3f7c162f18d
JR Rickerson <jrrickerson@redrivetstudios.com>
2023-01-19 11:48:39 -0500

    Day 9: Add Python part 2 solution and tests

--- python/day-09/solve.py
+++ python/day-09/solve.py
@@ -37,7 +37,40 @@ def part_1(input_data):
 
 
 def part_2(input_data):
-    pass
+    # Start Head at origin point
+    # Start Knots 1-9 at origin point
+    # Loop over commands, expanding them into individual "steps"
+    # Move Head according to each step
+    # After each step, update each knot position based on the knot
+    # before it
+    # Record each tail position (knot #9)
+    # Return count of unique positions
+    ORIGIN = utils.Vector2d(0, 0)
+    MAX_LEN = 1
+
+    head = utils.Vector2d(ORIGIN.x, ORIGIN.y)
+    knots = {num: utils.Vector2d(ORIGIN.x, ORIGIN.y) for num in range(0, 10)}
+
+    tail_log = set()
+    tail_log.add(knots[9])
+
+    for motion in input_data:
+        steps = utils.motion_to_steps(motion)
+        for step in steps:
+            head = utils.move_point(step, head)
+            # Make math consistent for following knots
+            knots[0] = head
+            for num in range(1, 10):
+                dist = utils.get_vector_distance(knots[num], knots[num - 1])
+                # See if the previous knot has moved far enough for this
+                # knot to need to move
+                if abs(dist.x) > MAX_LEN or abs(dist.y) > MAX_LEN:
+                    knot_step = utils.normalize_step(dist)
+                    knots[num] = utils.move_point(knot_step, knots[num])
+                    if num == 9:  # The actual "tail"
+                        tail_log.add(knots[num])
+
+    return len(tail_log)
 
 
 def main(input_file):  # pragma: no cover
--- python/day-09/test_solve.py
+++ python/day-09/test_solve.py
@@ -217,3 +217,37 @@ def test_part_1_sample_input():
     result = solve.part_1(input_data)
 
     assert result == 13
+
+
+def test_part_2_sample_input():
+    input_data = [
+        "R 4",
+        "U 4",
+        "L 3",
+        "D 1",
+        "R 4",
+        "D 1",
+        "L 5",
+        "R 2",
+    ]
+
+    result = solve.part_2(input_data)
+
+    assert result == 1
+
+
+def test_part_2_sample_input_2():
+    input_data = [
+        "R 5",
+        "U 8",
+        "L 8",
+        "D 3",
+        "R 17",
+        "D 10",
+        "L 25",
+        "U 20",
+    ]
+
+    result = solve.part_2(input_data)
+
+    assert result == 36
